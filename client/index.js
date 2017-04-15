c215ed1df0924ccc4081766a7ecc878ab49c6120
Fernando Fleury <me@fleury.io>
2017-04-15 13:56:35 -0300

    add css-modules

--- client/index.js
+++ client/index.js
@@ -3,23 +3,25 @@ import ReactDOM from 'react-dom'
 
 import { Provider } from 'react-redux'
 
+import store from 'app/store'
+
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
 
-import store from 'app/store'
+import Dummy from 'presentation/Dummy'
 
-const Dummy = () => (
+const App = () => (
   <Router>
-    <Route exact path="/" component={() => <div>Growler</div>} />
+    <Route exact path="/" component={Dummy} />
   </Router>
 )
 
 export default ReactDOM.render(
   <Provider store={store}>
-    <Dummy />
+    <App />
   </Provider>,
   document.body
 )
