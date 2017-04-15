924e326d8e396a008bb203ee84bda68e647fd3d1
Fernando Fleury <me@fleury.io>
2017-04-15 13:17:52 -0300

    add react-router as dep

--- client/index.js
+++ client/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Dummy = () => (
+  <Router>
+    <Route exact path="/" component={() => <div>Growler</div>} />
+  </Router>
+)
+
 export default ReactDOM.render(
-  <div>Grolwer</div>,
+  <Dummy />,
   document.body
 )
