edc337ccb9c52fc5c29e7a149a006c6b79e7b786
Fernando Fleury <me@fleury.io>
2017-04-16 01:45:04 -0300

    add eslint setup

--- client/app/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
-
-const sagaMiddleware = createSagaMiddleware()
-
-export default createStore(
-  () => {},
-  applyMiddleware(sagaMiddleware)
-)
--- client/presentation/Dummy/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import styles from './styles.css'
-
-const Dummy = () => <div className={styles.text}>Growler</div>
-
-export default Dummy
